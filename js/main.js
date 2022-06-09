666efdecba57efdc1db00a0465608d0db70e6daa
Aleksey <1aleksey.mazur@gmail.com>
2022-06-09 14:49:54 +0300

    _footer.scss completely done; changed gallery images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const myCarouselElement = document.querySelector("#carouselExampleCaptions");
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 100000,
+  interval: 5000,
   touch: true,
 });
