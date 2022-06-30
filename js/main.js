482ac08b359893320da9a05c7c8c1b826ccc9cd8
Aleksey <1aleksey.mazur@gmail.com>
2022-06-30 21:25:01 +0300

    nav id have been added and were tied with nav links; main.js:  added script for .active class replace while navigation have been switched

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 5000,
   touch: true,
 });
+
+const tabs = document.querySelectorAll(".nav-link");
+
+tabs.forEach((tab) => tab.addEventListener("click", toggleActiveTab));
+
+function toggleActiveTab(e) {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  e.currentTarget.classList.toggle("active");
+}
